4b750ee5467800d760aa953c440fdfaa7e2f803f
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-20 10:52:53 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import * as teacherHocs from './hocs/hocs';
 import * as studentHocs from './hocs/hocs2';
 
 
+const Meh = Container;
+const Improved = teacherHocs.withMagicProp(Meh);
 
 ReactDOM.render(
-  <Container />, document.querySelector('#target1'),
+  <Improved />, document.querySelector('#target1'),
 );
