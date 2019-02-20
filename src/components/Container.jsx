0d016603bec97e6de25acf37e36724f1a00a1727
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-20 12:38:16 +0100

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,7 +4,10 @@ export default function Container({ magic, timestamp, count, increment, decremen
   // stateful logic here
   return (
     <div className='container'>
-      Hello, World {magic} {timestamp} {count}
+      Hello, World
+      <div>magic prop: {magic}</div>
+      <div>timestamp: {timestamp}</div>
+      <div>Count: {count}</div>
 
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
