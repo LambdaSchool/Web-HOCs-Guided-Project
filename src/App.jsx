693f75fd2274f47c9c8ef7a3ff3a630d87ff5108
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-05 09:35:46 +0200

    simplify project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Container from './components/Container';
-import * as teacherHocs from './hocs/hocs';
-import * as studentHocs from './hocs/hocs2';
+import { render } from 'react-dom';
 
 
-const WrappedContainer = Container;
-
-ReactDOM.render(
-  <WrappedContainer />, document.querySelector('#target1'),
+render(
+  <div>Hello World</div>,
+  document.querySelector('#target1'),
 );
+
+export function withMagicProp(Component) {
+
+}
+
+
+// ================================================================ Exercise A
+
+// Build a HOC called `withTimestamp`
+// that provides the wrapped Component with a `timestamp` prop
+export function withTimestamp(Component) {
+
+}
+
+
+// ================================================================ Exercise B
+
+// Build a HOC called `withRandomNumberGenerator`
+// that provides the wrapped Component with a `generateRandomNumber` prop
+// which is a function that generates random integers from 0 to 9.
+export function withRandomNumberGenerator(Component) {
+
+}
+
+
+// ================================================================ Exercise C
+
+// Build a HOC called `withCounter`
+// that provides the wrapped Component with the following props:
+//   - a `count` which is a number starting at 0
+//   - an `incremement` which is a function that increments the counter by 1.
+//   - a `decrement` which is a function that decrements the counter by 1.
+export function withCounter(Component) {
+
+}
+
+
+// ================================================================ Exercise D
+
+// Build a HOC called `withAuthCheck`
+// that provides the wrapped component with an `isAuthed` prop which is a boolean
+// that informs whether there is an "auth" key with a value, inside local storage.
+export function withAuthCheck(Component) {
+
+}
